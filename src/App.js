ed371c25f7b1fd97436b95be4fc97bf7d0958ceb
pratham rai <97867973+pratham5368@users.noreply.github.com>
2023-12-22 20:27:02 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'
+
+function App() {
+  return (
+    <div>app</div>
+  );
+}
+
+export default App;
